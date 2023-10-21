877622054d29e5eb6f5c31558dc1507be263cb17
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-21 22:12:59 +0200

    implement saving jwt in local storage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,23 @@ import { useAuth } from '@/providers/AuthProvider'
 import { useGoogleLogin } from '@react-oauth/google'
 
 export const Navbar = () => {
-  const { login } = useAuth()
+  const { user, login, logout } = useAuth()
   const loginHandler = useGoogleLogin({
     onSuccess: ({ code }) => login(code),
     onError: () => console.log('error'),
     flow: 'auth-code',
   })
 
-  return (
-    <div>
-      Navbar:
-      <button className='border-2 px-5 py-2' onClick={() => loginHandler()}>
-        log in with google
+  return user ? (
+    <>
+      <div>user id: {user.user_id}</div>
+      <button className='border-2 px-5 py-2' onClick={() => logout()}>
+        log out
       </button>
-    </div>
+    </>
+  ) : (
+    <button className='border-2 px-5 py-2' onClick={() => loginHandler()}>
+      log in with google
+    </button>
   )
 }
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,12 +1,17 @@
 import { axiosClient } from '@/api/axios'
 import jwtDecode from 'jwt-decode'
-import { createContext, useContext, useMemo } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 
+const LS_KEY_JWT = 'jwt'
+
+type User = null | { user_id: number }
 type AuthContextValue = {
+  user: User
   login: (googleToken: string) => void
   logout: () => void
 }
 const AuthContext = createContext<AuthContextValue>({
+  user: null,
   login: () => {
     throw new Error('context is missing')
   },
@@ -17,23 +22,34 @@ const AuthContext = createContext<AuthContextValue>({
 
 type AuthProviderProps = { children: React.ReactNode }
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  // const [user, setUser] = useState<null>(null)
+  const [user, setUser] = useState<User>(null)
+
+  useEffect(() => {
+    const savedJwt = localStorage.getItem(LS_KEY_JWT)
+    const savedUser = savedJwt ? (jwtDecode(savedJwt) as User) : null
+    setUser(savedUser)
+  }, [])
 
   const login = async (googleCode: string) => {
     const response = await axiosClient.post('/accounts/google/login/', { code: googleCode })
     const token: string = response.data.access
 
-    console.log(jwtDecode(token))
+    localStorage.setItem(LS_KEY_JWT, token)
+    setUser(jwtDecode(token))
   }
 
-  const logout = () => {}
+  const logout = () => {
+    localStorage.removeItem(LS_KEY_JWT)
+    setUser(null)
+  }
 
   const value = useMemo(
     () => ({
+      user,
       login,
       logout,
     }),
-    [],
+    [user],
   )
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
