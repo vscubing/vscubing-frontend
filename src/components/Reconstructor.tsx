efd69fc4fe2e1c1671cda832748cd4d8341c71d8
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-10-24 12:52:06 +0200

    add importSolve to the reconstructor

--- src/components/Reconstructor.tsx
+++ src/components/Reconstructor.tsx
@@ -1,17 +1,23 @@
 import classNames from 'classnames'
-import { MouseEventHandler, useRef } from 'react'
+import { MouseEventHandler, useEffect, useRef } from 'react'
 
 export type ReconstructorSolve = { scramble: string; reconstruction: string }
 
 type ReconstructorProps = { solve: ReconstructorSolve | null; onClose: () => void }
 export const Reconstructor = ({ solve, onClose }: ReconstructorProps) => {
-  const iframeRef = useRef(null)
+  const iframeRef = useRef<HTMLIFrameElement | null>(null)
 
   const onBackgroundClick: MouseEventHandler = (event) => {
     if (event.target === event.currentTarget) {
       onClose()
     }
   }
+
+  useEffect(() => {
+    if (solve && iframeRef.current) {
+      importSolve(iframeRef.current, solve)
+    }
+  }, [solve])
 
   return (
     <div
@@ -21,4 +27,8 @@ export const Reconstructor = ({ solve, onClose }: ReconstructorProps) => {
       <iframe ref={iframeRef} className='rounded-[5px]' src='http://127.0.0.1:8080' width='1000' height='500'></iframe>
     </div>
   )
+}
+
+function importSolve(iframeElement: HTMLIFrameElement, solve: ReconstructorSolve) {
+  iframeElement.contentWindow?.postMessage({ source: 'vs-integration', solve }, 'http://127.0.0.1:8080')
 }
