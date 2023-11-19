bfe46ff5e3a49d51b89fc8c4e52dd74df8bd1a45
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-11-19 12:47:54 +0100

    add redirect to the default discipline from invalid discipline in route

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,5 +1,6 @@
 import { getOngoingContestNumber } from '@/api/contests'
 import { DEFAULT_DISCIPLINE } from '@/constants'
+import { isDiscipline } from '@/types'
 import { LoaderFunction, redirect } from 'react-router-dom'
 
 export const redirectToOngoingContest: LoaderFunction<null> = async () => {
@@ -10,7 +11,7 @@ export const redirectToOngoingContest: LoaderFunction<null> = async () => {
 export const redirectToDefaultDiscipline: LoaderFunction<null> = async ({ params }) => {
   const discipline = params.discipline
 
-  if (!discipline) {
+  if (!discipline || !isDiscipline(discipline)) {
     return redirect(DEFAULT_DISCIPLINE)
   }
   return null
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,7 @@
-export type Discipline = '3by3'
 export type Scramble = { scramble: string; position: string; extra: boolean; id: number }
+export type Discipline = (typeof DISCIPLINES)[number]
+
+const DISCIPLINES = ['3by3'] as const
+export const isDiscipline = (str: string): str is Discipline => {
+  return DISCIPLINES.includes(str as Discipline)
+}
