bec7351013d1cfbe7a095222631280733bb2f4a4
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-11-29 08:22:34 +0100

    add semantic header and main tags

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,13 +7,13 @@ export const Layout = () => {
     <>
       <PickUsernameModal />
       <div className='min-h-screen bg-background text-white'>
-        <div className='flex h-[50px] justify-between gap-4 border-b-[1px] border-white/10 px-5 lg:h-[55px] lg:px-20 xl:px-36'>
+        <header className='flex h-[50px] justify-between gap-4 border-b-[1px] border-white/10 px-5 lg:h-[55px] lg:px-20 xl:px-36'>
           <NavBar />
           <LoginSection />
-        </div>
-        <div className='px-5 py-8 lg:px-20 lg:py-16 xl:px-36 xl:py-24'>
+        </header>
+        <main className='px-5 py-8 lg:px-20 lg:py-16 xl:px-36 xl:py-24'>
           <Outlet />
-        </div>
+        </main>
       </div>
     </>
   )
