1fb25b2c71f7e3930f39717bdf036f1fe1cdf85b
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-24 20:14:32 +0100

    fix: axios interceptor refetch auth token only if has tokens

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,5 @@
 import { createAuthorizedRequestInterceptor, refreshAccessToken } from '@/features/auth'
+import { getAuthTokens } from '@/features/auth/authTokens'
 import axios from 'axios'
 import createAuthRefreshInterceptor from 'axios-auth-refresh'
 import applyCaseMiddleware from 'axios-case-converter'
@@ -14,6 +15,8 @@ const _axiosClient = axios.create(axiosParams)
 
 applyCaseMiddleware(_axiosClient)
 createAuthorizedRequestInterceptor(_axiosClient)
-createAuthRefreshInterceptor(_axiosClient, () => refreshAccessToken(axiosParams))
+createAuthRefreshInterceptor(_axiosClient, () => refreshAccessToken(axiosParams), {
+  shouldRefresh: (err) => err.response?.status === 401 && !!getAuthTokens(),
+})
 
 export const axiosClient = _axiosClient
