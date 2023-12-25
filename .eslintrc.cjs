b387a107ed0f750b155a39a6d0f6c99c729941ba
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-25 10:49:37 +0100

    refactor: make eslint config more strict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,19 @@ const config = {
   env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:@tanstack/eslint-plugin-query/recommended',
   ],
   ignorePatterns: ['dist', 'node_modules', '!.*.*'],
   parser: '@typescript-eslint/parser',
+  parserOptions: { project: true },
   plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
     '@typescript-eslint/consistent-type-imports': [
       'warn',
       {
@@ -20,6 +25,12 @@ const config = {
       },
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
   },
 }
 
