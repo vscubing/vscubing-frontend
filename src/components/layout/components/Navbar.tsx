aed4d2a0649ed83d46e30e873b3563b1774dda90
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-01-13 14:41:41 +0100

    fix: ongoing contest active highlight in navbar

--- src/components/layout/components/Navbar.tsx
+++ src/components/layout/components/Navbar.tsx
@@ -2,37 +2,48 @@ import { DashboardIcon, LeaderboardIcon, AllContestsIcon, OngoingContestIcon } f
 import { ongoingContestNumberQuery } from '@/features/contests'
 import { DEFAULT_DISCIPLINE } from '@/types'
 import { useQuery } from '@tanstack/react-query'
-import { Link, useMatchRoute, type LinkProps, type RegisteredRouter } from '@tanstack/react-router'
+import { Link, useMatchRoute, type LinkProps } from '@tanstack/react-router'
 
 export function Navbar() {
   const { data: ongoingContestNumber } = useQuery(ongoingContestNumberQuery)
   const matchRoute = useMatchRoute()
+  const isOnContests = !!matchRoute({
+    to: '/contests',
+    fuzzy: true,
+  })
+  const isOnOngoingContest = !!matchRoute({
+    to: '/contests/$contestNumber',
+    params: { contestNumber: String(ongoingContestNumber) },
+  })
+  const shouldHighlightAllContests = isOnContests && !isOnOngoingContest
 
   return (
     <nav className='flex flex-col gap-4 lg-short:gap-1'>
-      {getLinks(ongoingContestNumber, matchRoute).map(({ children, activeCondition = true, ...props }) => (
-        <Link
-          {...props}
-          key={props.to}
-          activeProps={
-            activeCondition
-              ? {
-                  className: 'text-primary-80 after:h-[1.5px] after:scale-x-100',
-                }
-              : undefined
-          }
-          className='title-h3 after-border-bottom transition-base outline-ring flex items-center gap-4 p-4 after:origin-[0%_50%] after:bg-primary-80 hover:text-primary-80 [&>svg]:h-6 [&>svg]:w-6'
-        >
-          {children}
-        </Link>
-      ))}
+      {getLinks(ongoingContestNumber, shouldHighlightAllContests).map(
+        ({ children, activeCondition = true, ...props }) => (
+          <Link
+            {...props}
+            key={props.to}
+            activeProps={
+              activeCondition
+                ? {
+                    className: 'text-primary-80 after:h-[1.5px] after:scale-x-100',
+                  }
+                : undefined
+            }
+            className='title-h3 after-border-bottom transition-base outline-ring flex items-center gap-4 p-4 after:origin-[0%_50%] after:bg-primary-80 hover:text-primary-80 [&>svg]:h-6 [&>svg]:w-6'
+          >
+            {children}
+          </Link>
+        ),
+      )}
     </nav>
   )
 }
 
 function getLinks(
   ongoingContestNumber: number | undefined,
-  matchRoute: ReturnType<typeof useMatchRoute<RegisteredRouter['routeTree']>>,
+  shouldHighlightAllContests: boolean,
 ): (LinkProps & { activeCondition?: boolean })[] {
   return [
     {
@@ -63,10 +74,7 @@ function getLinks(
         </>
       ),
       to: '/contests',
-      activeCondition: !matchRoute({
-        to: '/contests/$contestNumber',
-        params: { contestNumber: String(ongoingContestNumber) },
-      }),
+      activeCondition: shouldHighlightAllContests,
       params: {},
     },
     {
