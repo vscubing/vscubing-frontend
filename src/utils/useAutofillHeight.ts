dd7c5b80be8077563ec44dfee743646875d43eb6
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-02-25 20:42:57 +0100

    fix: useAutofillHeight min count 0

--- src/utils/useAutofillHeight.ts
+++ src/utils/useAutofillHeight.ts
@@ -1,15 +1,15 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import { flushSync } from 'react-dom'
 
 export function useAutofillHeight<
   TContainer extends HTMLElement = HTMLUListElement,
   TFake extends HTMLElement = HTMLLIElement,
->() {
+>(min = 1) {
   const containerRef = useRef<TContainer>(null)
   const fakeElementRef = useRef<TFake>(null)
   const [fittingCount, setFittingCount] = useState<number>()
 
-  function computeFittingAmount() {
+  const computeFittingAmount = useCallback(() => {
     flushSync(() => setFittingCount(undefined))
     const containerHeight = containerRef.current?.clientHeight
     const fakeElementHeight = fakeElementRef.current?.clientHeight
@@ -17,19 +17,18 @@ export function useAutofillHeight<
       setFittingCount(undefined)
       return
     }
+
     const gap = parseInt(getComputedStyle(containerRef.current).gap)
-    const count = Math.floor((containerHeight + gap - 1) / (fakeElementHeight + gap))
-    if (count === 0) {
-      setFittingCount(undefined)
-    } else {
-      setFittingCount(count)
-    }
-  }
+    let count = Math.floor((containerHeight + gap - 1) / (fakeElementHeight + gap))
+    count = Math.max(count, min)
+
+    setFittingCount(count)
+  }, [containerRef, fakeElementRef, min])
 
   useEffect(() => {
     addEventListener('resize', computeFittingAmount)
     setTimeout(() => computeFittingAmount(), 0) // setTimeout to suppress "flushSync was called from inside a lifecycle method" warning
     return () => removeEventListener('resize', computeFittingAmount)
-  }, [containerRef, fakeElementRef])
+  }, [containerRef, fakeElementRef, computeFittingAmount])
   return { fittingCount, containerRef, fakeElementRef }
 }
