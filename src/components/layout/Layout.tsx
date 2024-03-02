5ef1d26ba23687d746c31e38b857f4804fa7ea01
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-03-02 11:54:36 +0100

    fix: minor layout code style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,30 +1,37 @@
 import { Outlet } from '@tanstack/react-router'
 import { SocialLinks, Navbar, Logo, Copyright, LogoutButton } from './components'
 import { PickUsernameModal } from '../PickUsernameModal'
+import { cn } from '@/utils'
 
 export function Layout() {
   return (
     <>
       <PickUsernameModal />
       <div className='flex min-h-screen gap-3 p-[1.625rem]'>
-        <aside className='flex w-[clamp(16rem,20vw,21rem)] flex-col gap-3 md:hidden lg-short:min-w-[19rem]'>
-          <Logo className='h-[7rem] md:hidden lg-short:h-[4.375rem]' />
-          <div className='flex flex-1 flex-col justify-between rounded-2xl bg-black-80 py-6'>
-            <Navbar />
-            <div>
-              <div className='flex flex-col items-center gap-4 lg-short:flex-row lg-short:justify-center lg-short:gap-1'>
-                <SocialLinks />
-                {/* TODO: animate the social links to slide up after login before the log out button appears */}
-                <LogoutButton />
-              </div>
-              <Copyright className='mt-6' />
-            </div>
-          </div>
-        </aside>
+        <Sidebar className='md:hidden' />
         <main className='contents'>
           <Outlet />
         </main>
       </div>
     </>
+  )
+}
+
+function Sidebar({ className }: { className?: string }) {
+  return (
+    <aside className={cn('flex w-[clamp(16rem,20vw,21rem)] flex-col gap-3 lg-short:min-w-[19rem]', className)}>
+      <Logo className='h-[7rem] md:hidden lg-short:h-[4.375rem]' />
+      <div className='flex flex-1 flex-col justify-between rounded-2xl bg-black-80 py-6'>
+        <Navbar />
+        <div>
+          <div className='flex flex-col items-center gap-4 lg-short:flex-row lg-short:justify-center lg-short:gap-1'>
+            <SocialLinks />
+            {/* TODO: animate the social links to slide up after login before the log out button appears */}
+            <LogoutButton />
+          </div>
+          <Copyright className='mt-6' />
+        </div>
+      </div>
+    </aside>
   )
 }
