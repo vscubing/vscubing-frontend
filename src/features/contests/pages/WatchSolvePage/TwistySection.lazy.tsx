72c95d432eaf08ef3ce72af62034abdd7b27627e
bohdancho <bogdan.chornocondratenko@gmail.com>
2024-03-09 19:39:49 +0100

    feat(WatchSolvePage): tablet responsive

--- src/features/contests/pages/WatchSolvePage/TwistySection.lazy.tsx
+++ src/features/contests/pages/WatchSolvePage/TwistySection.lazy.tsx
@@ -29,18 +29,21 @@ function TwistySectionContent({ player, scramble }: { player: Player; scramble: 
 
   return (
     <>
-      <div className='flex flex-1 flex-col gap-10 rounded-2xl bg-black-80 pb-6'>
-        <TwistyPlayer player={player} className='flex-1' />
+      <div className='flex flex-col gap-10 rounded-2xl bg-black-80 pb-6 md:col-span-full'>
+        <TwistyPlayer player={player} className='flex-1 md:min-h-[20rem]' />
         <div className='flex flex-col items-center gap-2 px-14'>
           <TwistyScrubber twistyPlayer={player} className='w-full max-w-[25rem]' />
           <TwistyControls twistyPlayer={player} />
         </div>
       </div>
-      <div className='flex flex-col gap-3'>
-        <div className='flex flex-1 flex-col gap-3' ref={movesWrapperRef}>
+      <div className='flex flex-col gap-3 md:col-span-full md:flex-col-reverse'>
+        <div className='flex flex-1 grid-cols-2 flex-col gap-3 md:grid' ref={movesWrapperRef}>
           <div className='flex flex-col rounded-2xl bg-black-80 p-4'>
             <h2 className='title-h3 mb-2 border-b border-secondary-20 text-grey-20'>Scramble</h2>
-            <span className='title-h3 scrollbar basis-0 overflow-y-auto pr-2 tracking-wide' ref={scrambleWrapperRef}>
+            <span
+              className='title-h3 scrollbar basis-0 overflow-y-auto pr-2 tracking-wide md:basis-auto md:overflow-y-visible'
+              ref={scrambleWrapperRef}
+            >
               <span ref={scrambleRef}>{scramble}</span>
             </span>
           </div>
@@ -49,14 +52,16 @@ function TwistySectionContent({ player, scramble }: { player: Player; scramble: 
             <h2 className='title-h3 mb-2 border-b border-secondary-20 text-grey-20'>Solve</h2>
             <TwistyAlgViewer
               twistyPlayer={player}
-              className='title-h3 scrollbar flex-grow basis-0 overflow-y-auto pr-2 tracking-wide'
+              className='title-h3 scrollbar flex-grow basis-0 overflow-y-auto pr-2 tracking-wide md:basis-auto md:overflow-y-visible'
             />
           </div>
         </div>
 
-        <div className='rounded-2xl bg-black-80 p-4'>
-          <h2 className='title-h3 mb-1 text-grey-20'>Speed</h2>
-          <TwistyTempo twistyPlayer={player} />
+        <div className='rounded-2xl bg-black-80 p-4 md:flex md:justify-center'>
+          <div className='md:min-w-[25rem]'>
+            <h2 className='title-h3 mb-1 text-grey-20'>Speed</h2>
+            <TwistyTempo twistyPlayer={player} />
+          </div>
         </div>
       </div>
     </>
--- src/utils/mediaQueries.ts
+++ src/utils/mediaQueries.ts
@@ -2,21 +2,21 @@ import { rawTailwindConfig } from './tailwindConfig'
 
 export const isTouchDevice = matchMedia('(pointer:coarse)').matches
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-const screens = rawTailwindConfig.theme.extend.screens as Record<string, { raw?: string; max?: string; min?: string }>
-
+type Screen = keyof typeof rawTailwindConfig.theme.extend.screens
+type Query = { min?: string; max?: string } | { raw: string }
+const screens: Record<Screen, Query> = rawTailwindConfig.theme.extend.screens
 // WARNING: double check if it works before using
 export function matchesQuery(name: keyof typeof screens) {
   const data = screens[name]
-  if (data?.raw) {
+  if ('raw' in data) {
     return matchMedia(data.raw).matches
   }
 
   const query: string[] = []
-  if (data?.min) {
+  if ('min' in data) {
     query.push(`(min-width: ${data.min})`)
   }
-  if (data?.max) {
+  if ('max' in data) {
     query.push(`(max-width: ${data.max})`)
   }
   const queryString = query.join(' and ')
