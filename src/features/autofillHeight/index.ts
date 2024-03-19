bc6bc2e8bb8cf4b5a7cad7de5f9cf31efc93765f
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-19 22:16:23 +0100

    move useControllerWithInfiniteScroll to AutofillHeight

--- src/features/autofillHeight/index.ts
+++ src/features/autofillHeight/index.ts
@@ -1,5 +1,12 @@
 import { ListWrapper, ListWithPinnedItem, List } from './List'
 import { useFittingCount } from './useAutofillHeight'
+import { useControllerWithInfiniteScroll } from './useControllerWithInfiniteScroll'
 
 export type { ListWrapperProps, ListProps, ListWithPinnedItemProps } from './List'
-export const AutofillHeight = { ListWrapper, List, ListWithPinnedItem, useFittingCount }
+export const AutofillHeight = {
+  ListWrapper,
+  List,
+  ListWithPinnedItem,
+  useFittingCount,
+  useControllerWithInfiniteScroll,
+}
