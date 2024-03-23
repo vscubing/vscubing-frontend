3d9cc2ebe184c5b7d5929bc07e2f7d443f66a9df
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-23 11:29:02 +0100

    fix: add !pointer-events-auto for conflicting Radix Dialog (from Vaut) and Sonner toasts

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -77,8 +77,8 @@ function PopupMenu() {
   return (
     <Drawer.Root open={open} onOpenChange={setOpen} direction='right' shouldScaleBackground>
       <Drawer.Portal>
-        <Drawer.Overlay className='fixed inset-0 z-50 bg-black-1000/25 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0' />
-        <Drawer.Content className='fixed bottom-0 right-0 top-0 z-50'>
+        <Drawer.Overlay className='fixed inset-0 bg-black-1000/25 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0' />
+        <Drawer.Content className='fixed bottom-0 right-0 top-0'>
           <Sidebar className='h-full w-full bg-black-100 p-[1.625rem] sm:w-screen sm:p-3' />
         </Drawer.Content>
       </Drawer.Portal>
