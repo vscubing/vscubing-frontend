fffee0eb8f24f0d264d6fa943ab0b0b5f0886565
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-03-26 16:20:05 +0100

    fix: NotFoundPage remove alt's from decorative images

--- src/components/NotFoundPage/ParallaxCubes.lazy.tsx
+++ src/components/NotFoundPage/ParallaxCubes.lazy.tsx
@@ -26,33 +26,33 @@ function ParallaxCubes() {
   return (
     <>
       <MouseParallaxChild factorX={1.1} factorY={1.1} className='pointer-events-none absolute bottom-[15%] left-[10%]'>
-        <img src={SeparateLimeCube} alt='decorative cube' />
+        <img src={SeparateLimeCube} alt='' />
       </MouseParallaxChild>
       <MouseParallaxChild
         factorX={1.3}
         factorY={1.3}
         className='pointer-events-none absolute left-[10%] top-[max(40%,20rem)]'
       >
-        <img src={SeparatePurpleCube} alt='decorative cube' />
+        <img src={SeparatePurpleCube} alt='' />
       </MouseParallaxChild>
       <div className='pointer-events-none absolute left-1/2 top-[55%] aspect-square w-[min(80%,80vh)] -translate-x-1/2 -translate-y-1/2'>
         <MouseParallaxChild factorX={1.5} factorY={1.5} className='absolute right-[2%] top-[55%] w-[18%]'>
-          <img src={WhiteSmCube} alt='decorative cube' className='h-full w-full brightness-[60%]' />
+          <img src={WhiteSmCube} alt='' className='h-full w-full brightness-[60%]' />
         </MouseParallaxChild>
         <MouseParallaxChild className='absolute bottom-[5%] left-[30%] w-[21%]'>
-          <img src={BlackCube} alt='decorative cube' className='h-full w-full brightness-[60%]' />
+          <img src={BlackCube} alt='' className='h-full w-full brightness-[60%]' />
         </MouseParallaxChild>
         <MouseParallaxChild factorX={0.8} factorY={0.8} className='absolute bottom-[3%] left-[40%] w-[37%]'>
-          <img src={BlueCube} alt='decorative cube' className='h-full w-full brightness-[60%]' />
+          <img src={BlueCube} alt='' className='h-full w-full brightness-[60%]' />
         </MouseParallaxChild>
         <MouseParallaxChild factorX={1.2} factorY={1.2} className='absolute left-[25%] top-[40%] w-[40%]'>
-          <img src={WhiteCube} alt='decorative cube' className='h-full w-full brightness-[60%]' />
+          <img src={WhiteCube} alt='' className='h-full w-full brightness-[60%]' />
         </MouseParallaxChild>
         <MouseParallaxChild factorX={0.5} factorY={0.5} className='absolute right-0 top-0 w-[67%]'>
-          <img src={GreenCube} alt='decorative cube' className='h-full w-full brightness-[60%]' />
+          <img src={GreenCube} alt='' className='h-full w-full brightness-[60%]' />
         </MouseParallaxChild>
         <MouseParallaxChild factorX={2} factorY={4} className='pointer-events-auto absolute left-1/2 top-[40%] w-[80%]'>
-          <img src={Img404} alt='404 page not found' className='h-full w-full -translate-x-1/2' />
+          <img src={Img404} alt='' className='h-full w-full -translate-x-1/2' />
         </MouseParallaxChild>
       </div>
     </>
