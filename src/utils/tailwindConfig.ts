56b0832d6dc133405877f27226e12922a162cd1b
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-04-27 15:19:00 +0200

    refactor: put all tailwind related utils in one file

--- src/utils/tailwindConfig.ts
+++
@@ -1,5 +0,0 @@
-import resolveConfig from 'tailwindcss/resolveConfig'
-import rawConfig from '@/../tailwind.config.js'
-
-export const rawTailwindConfig = rawConfig
-export const tailwindConfig = resolveConfig(rawConfig)
