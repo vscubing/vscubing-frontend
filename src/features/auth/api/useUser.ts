29f5acf6702101edb4d69d70d8ad3362fce987a1
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-06-30 17:29:17 +0200

    fix: only fetch currentUser if authTokens exist

--- src/features/auth/api/useUser.ts
+++ src/features/auth/api/useUser.ts
@@ -1,10 +1,14 @@
 import { accountsCurrentUserRetrieve } from '@/api'
 import { queryOptions, useQuery } from '@tanstack/react-query'
 import { USER_QUERY_KEY } from '../userQueryKey'
+import { getAuthTokens } from '@/utils'
 
 export const userQuery = queryOptions({
   queryKey: [USER_QUERY_KEY],
-  queryFn: accountsCurrentUserRetrieve,
+  queryFn: () => {
+    if (!getAuthTokens()) return null
+    return accountsCurrentUserRetrieve()
+  },
 })
 
 export function useUser() {
