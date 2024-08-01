52fd7e4421dcc7d73ea9f32564ff84796da0240e
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-01 12:04:05 +0200

    fix: sync "Invalid page." expected response text with backend

--- src/utils/isInvalidPageError.ts
+++ src/utils/isInvalidPageError.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from 'axios'
 
+// this handles the case when the page number is greater than the total number of pages, so that we can redirect to the first page
 // NOTE: this should be used before checking for 404 status code
 export function isInvalidPageError(error: AxiosError | null) {
   const response = error?.response
@@ -8,6 +9,6 @@ export function isInvalidPageError(error: AxiosError | null) {
     response.data &&
     typeof response.data === 'object' &&
     'detail' in response.data &&
-    response.data.detail === 'Invalid Page'
+    response.data.detail === 'Invalid page.'
   )
 }
