52a801de263b19441d965a2fbbb9ba7265370145
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-14 18:04:18 +0200

    fix: add whitespace-nowrap to contest duration in ContestsIndexPage

--- src/features/contests/pages/ContestsIndexPage/Contest.tsx
+++ src/features/contests/pages/ContestsIndexPage/Contest.tsx
@@ -11,7 +11,7 @@ export function ContestRow({ contest, discipline, height }: ContestProps) {
       <span className='vertical-alignment-fix relative mr-4 flex-1 pr-4 after:absolute after:right-0 after:top-1/2 after:h-6 after:w-px after:-translate-y-1/2 after:bg-grey-60'>
         Contest {contest.name}
       </span>
-      <span className='vertical-alignment-fix mr-10 w-44'>{formatContestDuration(contest)}</span>
+      <span className='vertical-alignment-fix mr-10 w-44 whitespace-nowrap'>{formatContestDuration(contest)}</span>
       <SecondaryButton asChild className='h-full'>
         <Link
           to='/contests/$contestSlug/results'
