4ca19d12607e960ff63dead1d3daa53c65f2cbba
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-14 09:20:53 +0200

    feat: OngoingContestBanner mainenance case distinction

--- src/features/dashboard/components/OngoingContestBanner.tsx
+++ src/features/dashboard/components/OngoingContestBanner.tsx
@@ -6,17 +6,22 @@ import { cn } from '@/utils'
 import { Link } from '@tanstack/react-router'
 
 export function OngoingContestBanner() {
+  const isOnMaintenance = false
   return (
     <section className={cn('bg-card-gradient overflow-x-clip rounded-2xl')}>
-      <BannerContent className='lg:hidden' />
-      <BannerContentMobile className='hidden lg:flex' />
+      {isOnMaintenance ? (
+        <BannerOnMaintenance />
+      ) : (
+        <>
+          <BannerContent className='lg:hidden' />
+          <BannerContentMobile className='hidden lg:flex' />
+        </>
+      )}
     </section>
   )
 }
 
 function BannerContent({ className }: { className?: string }) {
-  const duration = useOngoingContestDuration()
-
   return (
     <div className={cn('flex', className)}>
       <div className='relative mr-32'>
@@ -29,13 +34,11 @@ function BannerContent({ className }: { className?: string }) {
 
       <div className='relative mr-32'>
         <div className='flex flex-col items-start justify-between gap-4 py-4 xl-short:pt-0'>
-          <h2 className='title-lg'>
-            <span className='text-secondary-20'>Ongoing</span> Contest
-          </h2>
+          <Title />
           <div className='flex w-full items-end justify-between'>
             <div>
               <p className='title-h3 mb-2'>Duration</p>
-              <Duration duration={duration} />
+              <Duration />
             </div>
             <PrimaryButton asChild>
               <Link search={{ discipline: DEFAULT_DISCIPLINE }} to='/contests/ongoing'>
@@ -46,22 +49,16 @@ function BannerContent({ className }: { className?: string }) {
         </div>
         <Divider className='absolute -right-40 top-0 h-full w-36' />
       </div>
-      <div className='relative flex-1 @container'>
-        <div className='absolute -left-10 top-0 h-full w-[calc(100%+20*(.25rem))] bg-[length:auto_100%] @[4rem]:bg-dashboard-banner-cubes @[24rem]:bg-dashboard-banner-cubes-wide'></div>
-      </div>
+      <ForegroundCubes />
     </div>
   )
 }
 
 function BannerContentMobile({ className }: { className?: string }) {
-  const duration = useOngoingContestDuration()
-
   return (
     <div className={cn('flex sm:flex-col sm:px-3 sm:py-4', className)}>
       <div className='relative z-10 py-4 pl-4 sm:flex sm:items-center sm:gap-4 sm:p-0'>
-        <h2 className='title-lg'>
-          <span className='text-secondary-20'>Ongoing</span> Contest
-        </h2>
+        <Title />
 
         <Popover>
           <PopoverContent>
@@ -81,7 +78,7 @@ function BannerContentMobile({ className }: { className?: string }) {
       <div className='flex flex-col items-end gap-6 py-4 pr-4 text-right sm:flex-row-reverse sm:items-center sm:justify-end sm:gap-2 sm:p-0 sm:text-left'>
         <div className='space-y-3 sm:space-y-1'>
           <p className='title-h3'>Duration</p>
-          <Duration duration={duration} />
+          <Duration />
         </div>
         <Disciplines />
       </div>
@@ -89,7 +86,23 @@ function BannerContentMobile({ className }: { className?: string }) {
   )
 }
 
-function Duration({ duration }: { duration: string | null }) {
+function BannerOnMaintenance() {
+  return (
+    <div className='flex gap-60'>
+      <div className='relative py-6 pl-4 sm:px-3 sm:py-4'>
+        <div>
+          <Title className='mb-3 sm:mb-0' />
+          <p className='text-large'>Currently down for maintenance</p>
+        </div>
+        <Divider className='absolute -right-28 top-0 h-full w-28 sm:hidden' />
+      </div>
+      <ForegroundCubes className='sm:hidden' />
+    </div>
+  )
+}
+
+function Duration() {
+  const duration = useOngoingContestDuration()
   return duration ? (
     <p className='text-large'>{duration}</p>
   ) : (
@@ -120,5 +133,21 @@ function Divider({ className }: { className?: string }) {
         className,
       )}
     ></div>
+  )
+}
+
+function Title({ className }: { className?: string }) {
+  return (
+    <h2 className={cn('title-lg', className)}>
+      <span className='text-secondary-20'>Ongoing</span> Contest
+    </h2>
+  )
+}
+
+function ForegroundCubes({ className }: { className?: string }) {
+  return (
+    <div className={cn('relative flex-1 @container', className)}>
+      <div className='absolute -left-10 top-0 h-full w-[calc(100%+20*(.25rem))] bg-[length:auto_100%] @[4rem]:bg-dashboard-banner-cubes @[20rem]:bg-dashboard-banner-cubes-wide'></div>
+    </div>
   )
 }
