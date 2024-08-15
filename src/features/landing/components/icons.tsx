c0fa282f88ce0940e32f575e5bb8df67fed1ea8c
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-15 13:05:42 +0200

    chore(landing): add icon assets

---
+++ src/features/landing/components/icons.tsx
@@ -0,0 +1,9 @@
+import CubeIcon from '../assets/cube.svg?react'
+import KeyboardIcon from '../assets/keyboard.svg?react'
+import LeaderboardIcon from '../assets/leaderboard.svg?react'
+import ResultIcon from '../assets/result.svg?react'
+import ScrambleIcon from '../assets/scramble.svg?react'
+import ShareIcon from '../assets/share.svg?react'
+import StopwatchIcon from '../assets/stopwatch.svg?react'
+
+export { CubeIcon, KeyboardIcon, LeaderboardIcon, ResultIcon, ScrambleIcon, ShareIcon, StopwatchIcon }
