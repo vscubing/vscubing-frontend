d65c63f533f9dce96bd58a851440eeaaa8991f2d
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-17 20:26:28 +0200

    feat(landing): virtual-cube-laptop WiP

--- src/features/landing/sections/AboutSection.tsx
+++ src/features/landing/sections/AboutSection.tsx
@@ -1,11 +1,15 @@
 import { CubeIcon, KeyboardIcon } from '../components/icons'
+import virtualCubeLaptopMp4 from '../assets/virtual-cube-laptop.mp4'
+import virtualCubeLaptopWebM from '../assets/virtual-cube-laptop.webm'
+import { useRef } from 'react'
+import { useIntersectionObserver } from 'usehooks-ts'
 
 export function AboutSection() {
   return (
-    <section>
-      <h2 className='landing-h2 mb-14 text-center'>What is virtual speedcubing?</h2>
+    <section className='mx-auto max-w-[75rem] space-y-14'>
+      <h2 className='landing-h2 text-center'>What is virtual speedcubing?</h2>
       <div className='flex justify-center gap-40'>
-        <div className='flex max-w-[30rem] items-center gap-2'>
+        <div className='flex items-center gap-2'>
           <CubeIcon className='flex-shrink-0' />
           <p>
             Speedcubing is an art of solving a Rubik's Cube as fast as possible. It’s a thrilling sport that challenges
@@ -20,6 +24,26 @@ export function AboutSection() {
           </p>
         </div>
       </div>
+      <CubeVideo />
     </section>
+  )
+}
+
+function CubeVideo() {
+  const { ref } = useIntersectionObserver({
+    threshold: 1,
+    freezeOnceVisible: true,
+    onChange: (isIntersecting) => {
+      if (isIntersecting) {
+        console.log('intersecting')
+      }
+    },
+  })
+
+  return (
+    <video width='100%' muted preload='none' className='rounded-3xl' ref={ref}>
+      <source src={virtualCubeLaptopWebM} type='video/webm; codecs=vp9' />
+      <source src={virtualCubeLaptopMp4} type='video/mp4' />
+    </video>
   )
 }
