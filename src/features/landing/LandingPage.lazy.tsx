ab870bf8073cf7d8230c90c783ccce5896054086
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-18 10:19:16 +0200

    feat(landing): FeaturesSection without animations

--- src/features/landing/LandingPage.lazy.tsx
+++ src/features/landing/LandingPage.lazy.tsx
@@ -3,6 +3,7 @@ import { Header } from './sections/Header'
 import { HeroSection } from './sections/HeroSection'
 import { Container } from './components/Container'
 import { AboutSection } from './sections/AboutSection'
+import { FeaturesSection } from './sections/FeaturesSection'
 
 export const Route = createLazyRoute('/landing')({
   component: LandingPage,
@@ -13,9 +14,10 @@ function LandingPage() {
     <>
       <Header />
       <main className='bg-black-120 pb-6 text-lg text-grey-40'>
-        <Container className='space-y-20'>
+        <Container className='space-y-40'>
           <HeroSection />
           <AboutSection />
+          <FeaturesSection />
         </Container>
       </main>
     </>
---
+++ src/features/landing/sections/FeaturesSection.tsx
@@ -0,0 +1,59 @@
+import { LeaderboardIcon, ResultIcon, ScrambleIcon, ShareIcon } from '../components/icons'
+import { type ReactElement } from 'react'
+
+export function FeaturesSection() {
+  return (
+    <section>
+      <h2 className='landing-h2 mb-14 text-center'>The problem we solve</h2>
+      <ul className='grid grid-cols-2 grid-rows-[18.75rem,18.75rem] gap-3'>
+        <Feature
+          title='Automated results'
+          description='No more manual entering of results in Excel. Everything is automated!'
+          renderIcon={() => <ResultIcon />}
+          renderVisualization={() => 'visual'}
+        />
+        <Feature
+          title='Instant scrambles'
+          description='Get your scrambles without any manual copy-pasting.'
+          renderIcon={() => <ScrambleIcon />}
+          renderVisualization={() => 'visual'}
+        />
+        <Feature
+          title='Leaderboards'
+          description='Compete for top spots on both contest-wise and all-time leaderboards.'
+          renderIcon={() => <LeaderboardIcon />}
+          renderVisualization={() => 'visual'}
+        />
+        <Feature
+          title='Share your solves'
+          description='Easily share your solves with friends and challenge them to beat your time!'
+          renderIcon={() => <ShareIcon />}
+          renderVisualization={() => 'visual'}
+        />
+      </ul>
+    </section>
+  )
+}
+
+function Feature({
+  title,
+  description,
+  renderIcon,
+  renderVisualization,
+}: {
+  title: string
+  description: string
+  renderIcon: () => ReactElement
+  renderVisualization: () => ReactElement
+}) {
+  return (
+    <li className='flex flex-col rounded-3xl bg-black-100 px-10 pb-10'>
+      <div className='flex flex-1 gap-28'>
+        <div className='self-end pb-6'>{renderIcon()}</div>
+        <div className='-mb-2 -mr-10 flex-1 border border-red-80'>{renderVisualization()}</div>
+      </div>
+      <h3 className='landing-h3 mb-2'>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
