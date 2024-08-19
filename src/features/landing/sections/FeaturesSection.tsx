cac0d1e4884aef2dd70630afaa83a4d29d279825
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-08-19 10:44:00 +0200

    refactor(landing): FeaturesSection pass components as props just as ReactNode

--- src/features/landing/sections/FeaturesSection.tsx
+++ src/features/landing/sections/FeaturesSection.tsx
@@ -1,6 +1,6 @@
+import { ReactNode } from '@tanstack/react-router'
 import { Container } from '../components/Container'
 import { LeaderboardIcon, ResultIcon, ScrambleIcon, ShareIcon } from '../components/icons'
-import { type ReactElement } from 'react'
 
 export function FeaturesSection({ className }: { className: string }) {
   return (
@@ -11,26 +11,26 @@ export function FeaturesSection({ className }: { className: string }) {
           <Feature
             title='Automated results'
             description='No more manual entering of results in Excel. Everything is automated!'
-            renderIcon={() => <ResultIcon />}
-            renderVisualization={() => 'visual'}
+            icon={<ResultIcon />}
+            visualization='visual'
           />
           <Feature
             title='Instant scrambles'
             description='Get your scrambles without any manual copy-pasting.'
-            renderIcon={() => <ScrambleIcon />}
-            renderVisualization={() => 'visual'}
+            icon={<ScrambleIcon />}
+            visualization='visual'
           />
           <Feature
             title='Leaderboards'
             description='Compete for top spots on both contest-wise and all-time leaderboards.'
-            renderIcon={() => <LeaderboardIcon />}
-            renderVisualization={() => 'visual'}
+            icon={<LeaderboardIcon />}
+            visualization='visual'
           />
           <Feature
             title='Share your solves'
             description='Easily share your solves with friends and challenge them to beat your time!'
-            renderIcon={() => <ShareIcon />}
-            renderVisualization={() => 'visual'}
+            icon={<ShareIcon />}
+            visualization='visual'
           />
         </ul>
       </section>
@@ -41,19 +41,19 @@ export function FeaturesSection({ className }: { className: string }) {
 function Feature({
   title,
   description,
-  renderIcon,
-  renderVisualization,
+  icon,
+  visualization,
 }: {
   title: string
   description: string
-  renderIcon: () => ReactElement
-  renderVisualization: () => ReactElement
+  icon: ReactNode
+  visualization: ReactNode
 }) {
   return (
     <li className='flex flex-col rounded-3xl bg-black-100 px-10 pb-10'>
       <div className='flex flex-1 gap-28'>
-        <div className='self-end pb-6'>{renderIcon()}</div>
-        <div className='-mb-2 -mr-10 flex-1 border border-red-80'>{renderVisualization()}</div>
+        <div className='self-end pb-6'>{icon}</div>
+        <div className='-mb-2 -mr-10 flex-1 border border-red-80'>{visualization}</div>
       </div>
       <h3 className='landing-h3 mb-2'>{title}</h3>
       <p>{description}</p>
