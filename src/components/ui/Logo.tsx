7eb2ece960e9dd9216283ab09e6675d7200791ca
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2024-12-23 20:00:46 +0100

    feat: add distinct logo element for dev and test environments, add build-test script

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef } from 'react'
 import logoFullImg from '@/assets/images/logo-full.svg'
 import logoSmImg from '@/assets/images/logo-sm.svg'
+import { cn } from '@/utils'
 
 const LOGO_VARIANTS = {
   full: logoFullImg,
@@ -9,7 +10,14 @@ const LOGO_VARIANTS = {
 
 export function Logo({
   variant = 'sm',
+  className,
   ...props
-}: ComponentPropsWithoutRef<'img'> & { variant?: keyof typeof LOGO_VARIANTS }) {
-  return <img src={LOGO_VARIANTS[variant]} alt='vscubing - Virtual Speedcubing' {...props} />
+}: ComponentPropsWithoutRef<'span'> & { variant?: keyof typeof LOGO_VARIANTS }) {
+  return (
+    <span {...props} className={cn('relative', className)}>
+      <img src={LOGO_VARIANTS[variant]} className='h-full w-full' alt='vscubing - Virtual Speedcubing' />
+      {import.meta.env.MODE === 'development' && <span className='title-h1 absolute top-0 text-white-100'>DEV</span>}
+      {import.meta.env.MODE === 'test' && <span className='title-h1 absolute top-0 text-white-100'>TEST</span>}
+    </span>
+  )
 }
