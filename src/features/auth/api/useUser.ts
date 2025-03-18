fcca1dbbd5c9cd6abdc405b240922665444013f6
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-18 08:37:23 +0100

    feat: add user context to sentry

--- src/features/auth/api/useUser.ts
+++ src/features/auth/api/useUser.ts
@@ -2,12 +2,15 @@ import { accountsCurrentUserRetrieve } from '@/api'
 import { queryOptions, useQuery } from '@tanstack/react-query'
 import { USER_QUERY_KEY } from '../userQueryKey'
 import { getAuthTokens } from '@/utils'
+import * as Sentry from '@sentry/react'
 
 export const userQuery = queryOptions({
   queryKey: [USER_QUERY_KEY],
-  queryFn: () => {
+  queryFn: async () => {
     if (!getAuthTokens()) return null
-    return accountsCurrentUserRetrieve()
+    const user = await accountsCurrentUserRetrieve()
+    Sentry.setUser({ username: user.username })
+    return user
   },
 })
 
