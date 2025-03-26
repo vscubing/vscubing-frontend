1ba646556bdac5260b4e46ac096175d0f7cc575e
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-26 17:26:27 +0100

    doEverything + split everything into smaller functions

--- src/shared/twisty/useTwistyPlayer.ts
+++ src/shared/twisty/useTwistyPlayer.ts
@@ -3,7 +3,7 @@ import { type PuzzleID, TwistyPlayer, Alg, LineComment, getSolveAnalyzer, Newlin
 import { puzzles } from '@vscubing/cubing/puzzles'
 import { useState, useEffect } from 'react'
 import { formatSolveTime } from '@/utils'
-import { Move, Pause } from '@vscubing/cubing/alg'
+import { type Move, Pause } from '@vscubing/cubing/alg'
 
 // TODO: validate timestamps beforehand
 
@@ -28,7 +28,7 @@ export function useTwistyPlayer({
         throw new Error(`invalid discipline: ${discipline}`)
       }
 
-      const animationLeaves = getAnimLeaves(solution)
+      const animationLeaves = await doEverything(scramble, solution)
 
       const newPlayer = new TwistyPlayer({
         controlPanel: 'none',
@@ -54,56 +54,61 @@ const TWISTY_PUZZLE_MAP: Record<Discipline, PuzzleID> = {
   '2by2': '2x2x2',
 }
 
-const ANALYZERS_MAP = {
-  '3by3': async (scramble, solutionWithTimings) => {
-    const cleanSolution = removeComments(solutionWithTimings)
-    const timings = solutionWithTimings
-      .split('*')
-      .filter((_, idx) => idx % 2 === 1)
-      .map(Number)
-    let lastIdx = -1
+async function doEverything(scramble: string, solutionWithTimings: string) {
+  const solution = new Alg(removeComments(solutionWithTimings))
+  const timings = solutionWithTimings
+    .split('*')
+    .filter((_, idx) => idx % 2 === 1)
+    .map(Number)
 
-    const puzzleLoader = puzzles['3x3x3']
-    const kpuzzle = await puzzleLoader.kpuzzle()
-    const solved = kpuzzle.defaultPattern()
-    const analyzeSolve = await getSolveAnalyzer(puzzleLoader)
+  const rawSignatures = await getSignatures(scramble, solution)
+  const signaturesWithDurations = embedDurations(rawSignatures, timings)
 
-    const fullSolutionAlg = new Alg(cleanSolution)
-    const fullSolutionNodes = [...fullSolutionAlg.childAlgNodes()]
+  const rawLeaves = constructLeaves(solution, timings)
+  const continuousLeaves = fillPauses(trimOverlapping(rawLeaves))
 
-    let curAlg = new Alg()
-    fullSolutionNodes.forEach((node, idx) => {
-      curAlg = new Alg([...curAlg.childAlgNodes(), node])
+  return annotateMoves(continuousLeaves, signaturesWithDurations)
+}
 
-      const pattern = solved.applyAlg(scramble).applyAlg(curAlg)
-      const signature = analyzeSolve(pattern)
-      if (!signature) {
-        return
-      }
+async function getSignatures(scramble: Alg | string, solution: Alg | string): Promise<(string | null)[]> {
+  const puzzleLoader = puzzles['3x3x3']
+  const kpuzzle = await puzzleLoader.kpuzzle()
+  const solved = kpuzzle.defaultPattern()
+  const analyzeSolve = await getSolveAnalyzer(puzzleLoader)
 
-      let comment = ' ' + signature
-      if (timings[idx]) {
-        const stepTime = lastIdx === -1 ? timings[idx] : timings[idx] - timings[lastIdx]
-        lastIdx = idx
-        comment += ` (${formatSolveTime(stepTime, true)}s)`
-      }
+  const signatures: (string | null)[] = []
+  let solutionSoFar = new Alg()
+  for (const node of new Alg(solution).childAlgNodes()) {
+    solutionSoFar = new Alg([...solutionSoFar.childAlgNodes(), node])
 
-      curAlg = new Alg([...curAlg.childAlgNodes(), new LineComment(comment), new Newline()])
-    })
-    return curAlg
-  },
-  '2by2': (_, solutionWithTimings) => {
-    const cleanSolution = removeComments(solutionWithTimings)
-    const fullSolutionAlg = new Alg(cleanSolution)
+    const pattern = solved.applyAlg(scramble).applyAlg(solutionSoFar)
+    signatures.push(analyzeSolve(pattern))
+  }
+  return signatures
+}
 
-    const alg = new Alg([
-      ...fullSolutionAlg.childAlgNodes(),
-      new Newline(),
-      new LineComment(' Analyzing 2x2 solves is not supported yet'),
-    ])
-    return new Promise((resolve) => resolve(alg))
-  },
-} satisfies Record<Discipline, (scramble: string, solutionWithTimings: string) => Promise<Alg>>
+function embedDurations(rawSignatures: (string | null)[], timings: number[]) {
+  let lastIdxWithSignature = -1
+  return rawSignatures.map((signature, idx) => {
+    if (signature === null) return null
+    const stepTime = lastIdxWithSignature === -1 ? timings[idx] : timings[idx] - timings[lastIdxWithSignature]
+    lastIdxWithSignature = idx
+    return ` ${signature} (${formatSolveTime(stepTime, true)}s)`
+  })
+}
+
+function constructLeaves(solution: Alg, timings: number[]): AnimationTimelineLeaf[] {
+  return [...solution.childAlgNodes()].map((algNode, idx) => {
+    const animLeaf = algNode
+    const start = timings[idx]
+    let end = start + 120
+    if (timings[idx + 1] === 0) {
+      end = 0
+    }
+
+    return { animLeaf, start, end }
+  })
+}
 
 function removeComments(moves: string): string {
   return moves
@@ -112,38 +117,104 @@ function removeComments(moves: string): string {
     .replaceAll('  ', ' ') // TODO: we shouldn't need this, investigate discrepancies between cstimer and phpless-cstimer
 }
 
-function getAnimLeaves(solutionWithTimings: string) {
-  const alg = Alg.fromString(removeComments(solutionWithTimings))
-  const algNodes = Array.from(alg.childAlgNodes())
+// NOTE: use original type once available
+type AnimationTimelineLeaf = { animLeaf: Pause | Move; start: number; end: number }
 
-  const timings = solutionWithTimings
-    .split('*')
-    .filter((_, idx) => idx % 2 === 1)
-    .map(Number)
+function trimOverlapping(leaves: AnimationTimelineLeaf[]): AnimationTimelineLeaf[] {
+  return leaves.map((leaf, idx) => {
+    const nextLeaf = leaves[idx]
+    if (!nextLeaf) return leaf
 
-  const noPauses = algNodes.map((algNode, idx) => {
-    const animLeaf = algNode
-    const start = timings[idx]
-    const end = start === 0 ? 0 : start + 120
-
-    return { animLeaf, start, end }
+    const end = Math.min(leaf.end, nextLeaf.end)
+    return { animLeaf: leaf.animLeaf, start: leaf.start, end }
   })
+}
 
-  const res: AnimationTimelineLeaf[] = [noPauses[0]]
-  for (let i = 1; i < noPauses.length; i++) {
-    const prev = noPauses[i - 1]
-    const cur = noPauses[i]
+function fillPauses(leaves: AnimationTimelineLeaf[]): AnimationTimelineLeaf[] {
+  const withPauses: AnimationTimelineLeaf[] = [leaves[0]]
+  for (let i = 1; i < leaves.length; i++) {
+    const prev = leaves[i - 1]
+    const cur = leaves[i]
 
-    if (prev.end >= cur.start) {
-      prev.end = cur.start
-    } else {
-      res.push({ animLeaf: new Pause(), start: prev.end, end: cur.start })
+    if (prev.end < cur.start) {
+      withPauses.push({ animLeaf: new Pause(), start: prev.end, end: cur.start })
     }
-    res.push(cur)
+    withPauses.push(cur)
   }
+  return withPauses
+}
 
+function annotateMoves(continuousLeaves: AnimationTimelineLeaf[], signaturesWithDurations: (string | null)[]) {
+  let realIdx = 0
+  const res: AnimationTimelineLeaf[] = []
+  for (const leaf of continuousLeaves) {
+    res.push(leaf)
+    if (leaf.animLeaf instanceof Pause) {
+      continue
+    }
+
+    const comment = signaturesWithDurations[realIdx]
+    if (comment !== null) {
+      res.push({ animLeaf: new LineComment(comment), start: leaf.start, end: leaf.end })
+      res.push({ animLeaf: new Newline(), start: leaf.start, end: leaf.end })
+    }
+    realIdx++
+  }
   return res
 }
 
-// NOTE: use original type once available
-type AnimationTimelineLeaf = { animLeaf: Pause | Move; start: number; end: number }
+function debugLeaves(animationLeaves: AnimationTimelineLeaf[]) {
+  animationLeaves.forEach((leaf) => {
+    console.log(leaf.start, leaf.end, leaf.animLeaf.toString())
+  })
+}
+// const ANALYZERS_MAP = {
+//   '3by3': async (scramble, solutionWithTimings) => {
+//     const cleanSolution = removeComments(solutionWithTimings)
+//     const timings = solutionWithTimings
+//       .split('*')
+//       .filter((_, idx) => idx % 2 === 1)
+//       .map(Number)
+//     let lastIdx = -1
+//
+//     const puzzleLoader = puzzles['3x3x3']
+//     const kpuzzle = await puzzleLoader.kpuzzle()
+//     const solved = kpuzzle.defaultPattern()
+//     const analyzeSolve = await getSolveAnalyzer(puzzleLoader)
+//
+//     const fullSolutionAlg = new Alg(cleanSolution)
+//     const fullSolutionNodes = [...fullSolutionAlg.childAlgNodes()]
+//
+//     let curAlg = new Alg()
+//     fullSolutionNodes.forEach((node, idx) => {
+//       curAlg = new Alg([...curAlg.childAlgNodes(), node])
+//
+//       const pattern = solved.applyAlg(scramble).applyAlg(curAlg)
+//       const signature = analyzeSolve(pattern)
+//       if (!signature) {
+//         return
+//       }
+//
+//       let comment = ' ' + signature
+//       if (timings[idx]) {
+//         const stepTime = lastIdx === -1 ? timings[idx] : timings[idx] - timings[lastIdx]
+//         lastIdx = idx
+//         comment += ` (${formatSolveTime(stepTime, true)}s)`
+//       }
+//
+//       curAlg = new Alg([...curAlg.childAlgNodes(), new LineComment(comment), new Newline()])
+//     })
+//     return curAlg
+//   },
+//   '2by2': (_, solutionWithTimings) => {
+//     const cleanSolution = removeComments(solutionWithTimings)
+//     const fullSolutionAlg = new Alg(cleanSolution)
+//
+//     const alg = new Alg([
+//       ...fullSolutionAlg.childAlgNodes(),
+//       new Newline(),
+//       new LineComment(' Analyzing 2x2 solves is not supported yet'),
+//     ])
+//     return new Promise((resolve) => resolve(alg))
+//   },
+// } satisfies Record<Discipline, (scramble: string, solutionWithTimings: string) => Promise<Alg>>
